16d3021544d0076856f28d7201b40ff61b12f309
Charles Hoshman <t.hoshman@gmail.com>
2017-09-25 10:08:12 -0700

    implemented string, number, boolean, and array case

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,35 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var returnStr = '';
+  
+  //string case
+  if(typeof obj === 'string'){
+    returnStr += '"' + obj + '"';
+  }
+  //number and boolean case
+  if(typeof obj === 'number' || typeof obj === 'boolean'){
+    returnStr += obj;
+  }
+
+  //array case
+  if(Array.isArray(obj)){
+    returnStr+= '[';
+    for(var i = 0; i < obj.length; i++){
+     returnStr += stringifyJSON(obj[i]);
+      returnStr += ',';
+    }
+    returnStr += ']';
+  }
+  
+
+  //object case
+
+
+
+
+
+
+  return returnStr;
 };
+
